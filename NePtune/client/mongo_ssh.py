af2bee2410f971ad720cf99085544afe7bb6e2dd
xll <xll@qq.com>
2022-10-28 01:13:17 -0400

    first

--- NePtune/client/mongo_ssh.py
+++ NePtune/client/mongo_ssh.py
@@ -2,41 +2,21 @@ from mongoengine import connect
 
 
 class MongoDB:
-    # host = '192.69.26.238'
-    # # host = '127.0.0.1'
-    # port = 30019
+    host = '192.69.26.238'
+    port = 30019
+    user = 'neptune'
+    dbname = 'NePtune'
+    passwd = 'neptune2022'
 
-    # user = 'neptune'
+    # host = '166.111.7.106'
+    # port = 30020
+    # user = 'xll'
     # dbname = 'NePtune'
-    # passwd = 'neptune2022'
-    # authentication_source = ''
+    # passwd = 'xllKEG2022'
 
-    # @classmethod
-    # def connect(cls):
-    #     connect(db=cls.dbname, host=cls.host, port=cls.port, username=cls.user, password=cls.passwd,
-    #             authentication_source="NePtune")
-
-
-    host = '127.0.0.1'
-    port = 27017
     authentication_source = ''
-    dbname = 'traffic'
-    dbname2 = 'userInfo'
-    alias = 'traffic'
-    alias2 = 'userInfo'
-
-    host_remote = '166.111.7.106'
-    port_remote = 30020
-    user = 'xll'
-    dbname_remote = 'NePtune'
-    passwd = 'xllKEG2022'
     alias_remote = 'NePtune'
-
     @classmethod
     def connect(cls):
-        # for local connection
-        connect(db=cls.dbname, host=cls.host, port=cls.port, authentication_source="traffic", alias=cls.alias)
-        connect(db=cls.dbname2, host=cls.host, port=cls.port, authentication_source="'userInfo'", alias=cls.alias2)
-        # for remote connection
-        connect(db=cls.dbname_remote, host=cls.host_remote, port=cls.port_remote, username=cls.user,
-                password=cls.passwd, authentication_source="NePtune", alias=cls.alias_remote)
+        connect(db=cls.dbname, host=cls.host, port=cls.port, username=cls.user, password=cls.passwd,
+                authentication_source="NePtune",alias=cls.alias_remote)
